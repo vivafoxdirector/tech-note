b5fed62d0b0741233fd47ba0017605e98531a882
VivaFoxdirector <vivafoxdirector@gmail.com>
2021-02-17 17:28:11 +0900

    20210217

--- bizchat/유지보수/TASK2080/bizchat1074_table.sql
+++ bizchat/유지보수/TASK2080/bizchat1074_table.sql
@@ -82,3 +82,76 @@ ALTER TABLE COM_CD ADD (
 ALTER TABLE ROLE_RSRC_MAPP ADD (
      BLC_ID NUMBER -- Admin사업자ID 
 );
+
+-- 이벤트 테이블 컬럼 추가
+ALTER TABLE EVENT_MGMT ADD (
+    BLC_ID NUMBER -- 사업자 ID
+);
+
+-- 문서 다운로드 요청 테이블 컬럼 추가
+ALTER TABLE DOC_DL_REQ ADD (
+    BLC_ID NUMBER -- 사업자 ID
+);
+
+
+
+-- # DML
+1. 오비즈사업 관련 데이터를 입력하기 전에 아래와 같은 설정이 우선되어야 한다.
+
+-- ## BLC 테이블 입력
+-- 1. 비즈챗 사업 => BLC_ID : 1
+-- 주의사항: 반드시 'BLC_'로 시작하는 문구로 한다. 로직/쿼리와 연관된다.
+insert into blc (
+    blc_id, blc_cd, blc_nm, crt_dttm, crt_user_id, crt_prgm_id, upd_dttm, upd_user_id, upd_prgm_id, blc_disp_nm
+) values (1,'BLC_BIZCHAT','비즈챗(BIZCHAT)',SYSDATE,'INIT','INIT',SYSDATE,'INIT','INIT','비즈챗');
+-- 2. 오비즈 사업 => BLC_ID : 10
+insert into blc (
+    blc_id, blc_cd, blc_nm, crt_dttm, crt_user_id, crt_prgm_id, upd_dttm, upd_user_id, upd_prgm_id, blc_disp_nm
+) values (10,'BLC_OBIZ','오비즈(OBIZ)',SYSDATE,'INIT','INIT',SYSDATE,'INIT','INIT','오비즈');
+
+-- ## RLC 테이블
+-- 1. 기존 비즈챗 blcid로 전부 세팅한다.
+update rlc set blc_id = 1;
+
+-- ## ADMIN 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update admin set blc_id = 1;
+
+-- ## CUSTOM 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update custom set blc_id = 1;
+
+-- ## SEP_CUSTOM 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update sep_custom set blc_id = 1;
+
+-- ## NOTI 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update noti set blc_id = 1;
+
+-- ## FAQ 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update faq set blc_id = 1;
+
+-- ## QNA 테이블
+-- 1. 기존 비즈챗 blcid로 전부 설정
+update qna set blc_id = 1;
+
+-- ## COM_CD 테이블
+-- 1. 공통 코드에서 코드 종류 'RLC_TP_CD' 인것만 세팅
+update com_cd set blc_id = 1 where cd_grp_id = 'RLC_TP_CD'
+-- 2. 오비즈 중계사도 입력을 하도록 한다. (주의: blc_id 충돌이 안나야 한다.)
+
+-- ## EVENT_MGMT 테이블
+-- 1. 공통 코드에서 코드 종류 'RLC_TP_CD' 인것만 세팅
+update EVENT_MGMT set blc_id = 1;
+
+-- ## EVENT_MGMT 테이블
+-- 1. 공통 코드에서 코드 종류 'RLC_TP_CD' 인것만 세팅
+update DOC_DL_REQ set blc_id = 1;
+
+
+
+-- ## ROLE 테이블 (오비즈용 롤 생성)
+-- ## RSRC 테이블 (오비즈용 리소스 생성)
+-- ## ROLE_RSRC_MAPP 테이블 (오비즈용 맵핑 생성)
